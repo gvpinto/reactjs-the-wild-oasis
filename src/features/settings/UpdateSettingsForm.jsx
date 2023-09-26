012acfac778db593263b4a552fd2146292e69039
Glenn Pinto <gvpinto@gmail.com>
2023-09-25 21:11:40 -0400

    360. Updating Application Settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -3,8 +3,10 @@ import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
 import Spinner from '../../ui/Spinner';
 import { useSettings } from './useSettings';
+import { useUpdateSetting } from './useUpdateSetting';
 
 function UpdateSettingsForm() {
+  const { isUpdating, updateSetting } = useUpdateSetting();
   const {
     isLoading,
     settings: {
@@ -15,6 +17,11 @@ function UpdateSettingsForm() {
     } = {},
   } = useSettings();
 
+  function handleUpdate(e, field) {
+    const { value } = e.target;
+    updateSetting({ [field]: value });
+  }
+
   if (isLoading) return <Spinner />;
 
   return (
@@ -24,6 +31,8 @@ function UpdateSettingsForm() {
           type='number'
           id='min-nights'
           defaultValue={minBookingLength}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, 'minBookingLength')}
         />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
@@ -31,6 +40,8 @@ function UpdateSettingsForm() {
           type='number'
           id='max-nights'
           defaultValue={maxBookingLength}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, 'maxBookingLength')}
         />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
@@ -38,6 +49,8 @@ function UpdateSettingsForm() {
           type='number'
           id='max-guests'
           defaultValue={maxGuestsPerBooking}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, 'maxGuestsPerBooking')}
         />
       </FormRow>
       <FormRow label='Breakfast price'>
@@ -45,6 +58,8 @@ function UpdateSettingsForm() {
           type='number'
           id='breakfast-price'
           defaultValue={breakfastPrice}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, 'breakfastPrice')}
         />
       </FormRow>
     </Form>
